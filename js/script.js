5b2a05f80a015037e4050fd77e744050ed8fa075
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-12 16:47:25 +0200

    adding images with vue

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@ const { createApp } = Vue
 
   createApp({
     data() {
-       images = [
-         "./imgs/01.webp",
-         "./imgs/02.webp",
-         "./imgs/03.webp",
-         "./imgs/04.webp",
-         "./imgs/05.webp",
-       ]
-      
+      return { 
+        images: [
+        "./imgs/01.webp",
+        "./imgs/02.webp",
+        "./imgs/03.webp",
+        "./imgs/04.webp",
+        "./imgs/05.webp",
+      ],
+      activeIndex : 0,
+    } 
+
+
+    
+       
     },
      
      
